560eeaf386e4806c1b002975eebe05e331e2395b
Anton Kulikov <avk@paleblue.no>
2018-01-09 19:04:56 +0300

    Transited mod to 0.16 ver Factorio

--- prototypes/item/item.lua
+++ prototypes/item/item.lua
@@ -4,9 +4,11 @@ data:extend(
 		type = "item",
 		name = "exoskeleton-mark-ii",
 		icon = "__Exoskeleton Mark II__/graphics/exoskeleton-mark-ii.png",
+		icon_size = 32,
 		placed_as_equipment_result = "exoskeleton-mark-ii",
 		flags = {"goes-to-main-inventory"},
-		group = "combat", subgroup = "equipment",
+		group = "combat", 
+		subgroup = "equipment",
 		order = "e[exoskeleton]-a[exoskeleton-mark-ii]",
 		stack_size = 4
 	}
