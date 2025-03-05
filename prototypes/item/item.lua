b22ba28d9e2328e4d37e9dd9eceb92dda60cbb08
axtox <axtoox@protonmail.com>
2025-03-06 00:21:17 +0300

    Item data model updated to the latest version There was a problem with 'placed_as_equipment_result' field since it was renamed to 'place_as_equipment_result' and wasn't working as expected

--- prototypes/item/item.lua
+++ prototypes/item/item.lua
@@ -6,7 +6,7 @@ data:extend(
 		type = "item",
 		name = "exoskeleton-mark-ii",
 		icon = "__Exoskeleton Mark II__/graphics/icons/exoskeleton-mark-ii-equipment.png",
-		placed_as_equipment_result = "exoskeleton-mark-ii",
+		place_as_equipment_result = "exoskeleton-mark-ii",
 		subgroup = "utility-equipment",
 		order = "d[exoskeleton]-a[exoskeleton-mark-ii]",
 		inventory_move_sound = item_sounds.exoskeleton_inventory_move,
